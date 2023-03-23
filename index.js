ea66b575bcb8b443108330fa38d32877ff210ca1
timmyades3 <oluwatimileyinadesina623@gmail.com>
2023-03-23 02:06:53 +0100

    changes

--- index.js
+++ index.js
@@ -1,38 +1,43 @@
-// Get all the list items in the sidebar
 const sidebarItems = document.querySelectorAll('.sidebar li');
 const bars = document.querySelector('#bars');
 const remove = document.querySelector('#remove');
-// Add a click event listener to each list item
-sidebarItems.forEach(item => {
-  item.addEventListener('click', () => {
-    // Find the sidebar element
-    const sidebar = document.querySelector('.sidebar');
+
+const mediaQuery = window.matchMedia('(max-width: 991px)');
+
+function handleMediaQuery(mediaQuery) {
+  if (mediaQuery.matches) {
     
+    sidebarItems.forEach(item => {
+      item.addEventListener('click', () => {
+        const sidebar = document.querySelector('.sidebar');
+        sidebar.style.right = '-1000px';
+        remove.style.display = 'none';
+        bars.style.display = 'block';
+      });
+    });
 
-    // Hide the sidebar
-    sidebar.style.right = '-1000px';
-    remove.style.display = 'none';
-    bars.style.display = 'block'
+    bars.addEventListener('click', () => {
+      const sidebar = document.querySelector('.sidebar');
+      sidebar.style.right = '0px';
+      remove.style.display = 'block';
+      bars.style.display = 'none';
+    });
+
+    remove.addEventListener('click', () => {
+      const sidebar = document.querySelector('.sidebar');
+      sidebar.style.right = '-1000px';
+      remove.style.display = 'none';
+      bars.style.display = 'block';
+    });
+  } else {
    
-  });
-});
+    remove.style.display = 'none';
+    bars.style.display = 'none';
+  }
+}
 
-bars.addEventListener('click', () => {
-  const sidebar = document.querySelector('.sidebar');
-
-  sidebar.style.right = '0px';
-  remove.style.display = 'block';
-  bars.style.display = 'none';
-});
-
-remove.addEventListener('click', () => {
-  const sidebar = document.querySelector('.sidebar');
-
-  sidebar.style.right = '-1000px';
-  remove.style.display = 'none';
-  bars.style.display = 'block';
-});
-
+handleMediaQuery(mediaQuery);
+mediaQuery.addListener(handleMediaQuery);
 
 
 
